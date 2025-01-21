1aa9208b727988f22e9ee160eb16ef24de6f49dc
Michael <michaeldam.dev@gmail.com>
2025-01-21 10:29:00 -0700

    Updated Table of Contents

--- src/app/projects/[uid]/Contents.tsx
+++ src/app/projects/[uid]/Contents.tsx
@@ -12,18 +12,17 @@ export default function Contents({ data }: any) {
     // console.log(entry)
     return (
         <>
-            <div className="fixed bottom-4 left-4 z-10 rounded lg:sticky lg:top-[112px] min-w-fit h-fit bg-black2 border border-primary">
+            <div className="fixed bottom-4 left-4 z-10 rounded lg:sticky lg:top-[128px]  h-fit bg-black2 border-primary border">
 
                 <div className='flex items-center justify-start gap-2 p-4 text-h4' onClick={() => (setOpen(!open))}>
                     <IoIosList />
                     <h2 className='hidden lg:block'>
                         Contents
                     </h2>
-
                 </div>
 
-                <div className={` grid transition-all  ${open ? "grid-rows-1 pb-4 " : "grid-rows-0"} lg:grid-rows-1 ml-4 lg:mt-2 `}>
-                    <ul className={`list-disc list-inside marker:text-primary w-fit overflow-hidden`}>
+                <div className={`grid w-0 ${open ? "grid-rows-1 w-max pb-4" : "grid-rows-0"} lg:grid-rows-1  px-4 lg:pb-4`}>
+                    <ul className={`list-disc list-inside marker:text-primary w-fit overflow-hidden ml-4`}>
                         {data.map((item: any, index: number) => {
                             let heading = ""
                             if (item.slice_type == 'screenshots') {
